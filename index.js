12e2dca7dfe79083c5bd0f554f8d7dd5eef5dee7
sos171810fgg <bolodelaus@gmail.com>
2018-03-08 16:31:39 +0000

    4 cambio

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express=require("express");
 //var cool =require("cool-ascii-faces");
+var port = (process.env.PORT || 1607)
+
 var app=express();
+
+app.use("/",express.static(__dirname+"/public"));
 
 app.get("/hello",(req,res)=>{
     res.send("Hello world!");
 });
 
-app.listen(process.env.PORT);
+app.get("/time",(req,res)=>{
+    console.log("new request to /time");
+    res.send(new Date());
+})
+
+app.listen(port,()=>{
+    console.log("Server Ready on port" +port+ "!");
+}).on("error",(e)=>{
+    console.log("Server NOT READY:" +e+ "!");
+});
 
 //console.log(cool());
+console.log("Server setting up....");
