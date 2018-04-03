1700e239f9abe2c993e37cdfe933e55fe95cb3a9
sos171810dbd <davidbonilla37@gmail.com>
2018-04-03 10:24:44 +0000

    modiicacion

--- apis/builders.js
+++ apis/builders.js
@@ -229,42 +229,39 @@ exports.register = function(app, dbd, BASE_API_PATH) {
 
     ///////////////////////////////////PUT A UN RECURSO (ACTUALIZA EL RECURSO)////////////////////////////////////////////////////
     app.put(BASE_API_PATH + "/builders/:year", (req, res) => {
-        var year = req.params.year;
-        var updatedBuilder= req.body;
-        
-        console.log(Date() + " - PUT /builders/" + year);
-        
-        if (!updatedBuilder) {
-            console.log("WARNING: New PUT request to /builders/ without builder, sending 400...");
-            res.sendStatus(400); // bad request
-            return
-        }else{
-            console.log("INFO: New PUT request to /builders/" + year + " with data " + updatedBuilder);
-            if(updatedBuilder.year!=year){
-                console.log("WARNING: New PUT request to /lfppichichitrophy/ with diferent season, sending 400...");
+            var year = req.params.year;
+            var updatedBuilder = req.body;
+
+            console.log(Date() + " - PUT /builders/" + year);
+
+            if (!updatedBuilder || updatedBuilder.year != year) {
+                console.log("WARNING: New PUT request to /builders/ without builder or with different year sending 400...");
                 res.sendStatus(400); // bad request
-        
-            }else{
+                return
+            }
+            else {
+                console.log("INFO: New PUT request to /builders/" + year + " with data " + updatedBuilder);
                 dbd.find({ "year": parseInt(year) }).toArray((err, filteredBuilders) => {
                     if (err) {
                         console.error('WARNING: Error getting data from DB');
                         res.sendStatus(500); // internal server error
                         return
-                    }else{
+                    }
+                    else {
                         if (filteredBuilders.length > 0) {
-                            dbd.update({"year": parseInt(year)}, updatedBuilder);
-                            res.sendStatus(200);//Modified
+                            dbd.update({ "year": parseInt(year) }, updatedBuilder);
+                            console.log("auqn da fallo lo modifica");
+                            res.sendStatus(200); //Modified
                         }
                         else {
                             console.log("WARNING: There are not any contact with builder " + year);
                             res.sendStatus(404); // not found
-        
+
                         }
                     }
-        
+
                 });
-            }    
-        }
+            }
     });
 
 }
