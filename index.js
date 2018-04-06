974c5c761ea93d4170311b128574f7a3b7da9dce
sos171810fgg <bolodelaus@gmail.com>
2018-04-06 19:25:33 +0000

    el bolo sigue probando

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ var bodyParser = require("body-parser");
 
 /////////////////////////MÓDULOS DE APIS//////////////////////////
 ///////API DAVID///////////////////
-var apiBuilders = require( "./apis/builders.js");
+var apiBuilders = require("./apis/builders.js");
 ///////API PACO/////////////////////
-var apiMotogpStats = require( __dirname + "/apis/motogp-stats.js");
+var apiMotogpStats = require(__dirname + "/apis/motogp-stats.js");
 ///////API VICTOR//////////////////
-var apiBuses = require ( __dirname + "/apis/buses.js");
+var apiBuses = require(__dirname + "/apis/buses.js");
 
 var security = require("./security.js")
 
 var app = express();
 app.use(bodyParser.json());
 app.use("/", express.static(__dirname + "/public"));
+
 
 ////////CONEXION BASE DE DATOS//////////////////////////////////////////////////
 var MongoClient = require("mongodb").MongoClient;
@@ -25,7 +26,7 @@ var BASE_API_PATH = "/api/v1";
 var BASE_API_PATH_SECURE = "/api/v1/security";
 
 MongoClient.connect(mdbURL, { native_parser: true }, (err, mlabs) => {
-    
+
     if (err) {
         console.error("Error accesing DB: " + err);
         process.exit(1);
@@ -42,22 +43,25 @@ MongoClient.connect(mdbURL, { native_parser: true }, (err, mlabs) => {
     apiBuilders.register(app, dbd, BASE_API_PATH, security.checkApiKeyFunction);
     apiMotogpStats.register(app, dbp, BASE_API_PATH, security.checkApiKeyFunction);
     apiBuses.register(app, db, BASE_API_PATH, security.checkApiKeyFunction);
-    
-     app.listen(port, () => {
+
+    app.listen(port, () => {
         console.log("Server Ready on port" + port + "!");
     }).on("error", (e) => {
         console.log("Server NOT READY:" + e + "!");
     });
-    
+
     console.log("Server setting up....");
 });
 
 
-     ////// probando //////
-     
-    app.get(BASE_API_PATH_SECURE , (req,res) => {
-    if(!security.checkApiKeyFunction(req,res))return;
-    });
-    
+////// probando //////
+
+app.get(BASE_API_PATH_SECURE, (req, res) => {
+    if (!security.checkApiKeyFunction(req, res)) return;
+    console.log(Date() + " - GET /security");
+    res.open(app.use("/", express.static(__dirname + "/public_security")));
+    console.log(Date() + "MOSTRANDO PAGINA");
+});
+
 
 ////////////////////////////////////////////////////////////////////////////////
--- security.js
+++ security.js
@@ -1,21 +1,21 @@
-var express = require("express");
-var app = express();
 var security = {};
+
 module.exports = security;
-app.use("/", express.static(__dirname + "/public_security"));
+
 var api_key = "davvicfra";
 
-security.checkApiKeyFunction = function(req,res) {
-    
-    if(!req.query.apikey){
+security.checkApiKeyFunction = function(req, res) {
+
+    if (!req.query.apikey) {
         console.error("WARNING: No apikey");
         res.sendStatus(401);
         return false;
     }
-    if(req.query.apikey !== api_key){
+    if (req.query.apikey !== api_key) {
         console.error("WARNING: Incorrect apikey was used, churrita!");
         res.sendStatus(403);
         return false;
     }
     return true;
 };
+
