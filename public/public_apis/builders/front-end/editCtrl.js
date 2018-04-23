27745fb0e518f932496af48d5228f689c21ee7e5
sos171810dbd <davidbonilla37@gmail.com>
2018-04-23 09:20:14 +0000

    df

--- public/public_apis/builders/front-end/editCtrl.js
+++ public/public_apis/builders/front-end/editCtrl.js
@@ -1,16 +1,20 @@
 /* global angular */
 
- angular.module("BuildersApp").controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
-     console.log("Edit Ctrl initialized!");
-     var builderURL = "/api/v1/builders/" + $routeParams.year;
-     
-     $http.get(builderURL).then(function(response){
-           $scope.updatedBuilder = response.data;
-     });
-     $scope.updateBuilder = function() {
-     $http.put(builderURL, $scope.updatedBuilder).then(function(response) {
+angular.module("BuildersApp").controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
+ console.log("Edit Ctrl initialized!");
+ var builderURL = "/api/v1/builders/" + $routeParams.year;
+
+ $http.get(builderURL).then(function(response) {
+  $scope.updatedBuilder = response.data;
+ });
+ $scope.updateBuilder = function() {
+  if(Object.values($scope.updatedBuilder).includes(null)){
+          $scope.status = "FAIL: It is necesary to fill in all the fields --> status: (400)";
+      }else{
+        $http.put(builderURL, $scope.updatedBuilder).then(function(response) {
            $scope.status = "Status:" + response.status;
            $location.path("/");
-     });
-   }
- }]);
+  });
+ }
+ };
+}]);
--- public/public_apis/builders/front-end/listCtrl.js
+++ public/public_apis/builders/front-end/listCtrl.js
@@ -5,28 +5,42 @@ angular.module("BuildersApp").controller("ListCtrl", ["$scope", "$http", functio
     var api = "/api/v1/builders";
 
     $scope.addBuilder = function() {
-        $http.post(api, $scope.newBuilder).then(function(response) {
-
-            $scope.status = "Status:" + response.status;
+        $http.post(api, $scope.newBuilder).then(function successCallback(response) {
+            //$scope.status = "Status:" + response.status;
             console.log(JSON.stringify((response, null, 2)));
             getBuilders();
+            },function errorCallback(response){
+            console.log(response.status);
+            if(response.status == 409){
+                $scope.status = "Status:" + response.status + ("FAIL: Builder already exist!");
+            }
+            if(response.status == 422){
+                $scope.status = "Status:" + response.status + ("FAIL: Builder does not have expected fields!");
+            }
+            if(response.status == 400){
+                $scope.status == "Status:" + response.status + ("FAIL: New POST request to /builder/ without builder" );
+            }
         });
+        getBuilders();
     }
 
     $scope.deleteBuilder = function(year) {
         $http.delete(api + "/" + year).then(function(response) {
-            
-            $scope.status = "Status:" + response.status;
+            //$scope.status = "Status:" + response.status;
             console.log(JSON.stringify((response, null, 2)));
             getBuilders();
         });
     }
     
     $scope.deleteAll = function(){
-        $http.delete(api).then(function(response){
+        $http.delete(api).then(function successCallback(response){
             
-            $scope.status = "Status:" + response.status;
+            //$scope.status = "Status:" + response.status;
             console.log("Lista Vacia");
+            getBuilders();
+        }, function errorCallback(response){
+            $scope.status = "Status:" + response.status + "(FAIL: you can not delete all pilots)";
+            console.log("ERROR");
             getBuilders();
         });
     }
