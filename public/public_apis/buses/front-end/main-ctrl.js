079f470fc0fe6bc64b56436ff7892526f0d2ef01
sos171810vcm <viccanmag@alum.us.es>
2018-05-11 15:13:01 +0000

    v

--- public/public_apis/buses/front-end/main-ctrl.js
+++ public/public_apis/buses/front-end/main-ctrl.js
@@ -97,26 +97,29 @@ angular
                         
                        
 
-                        var myChart = eCharts.init(document.getElementById('main'));
+                        var myChart = echarts.init(document.getElementById('main'));
 
                         
 
                         var option = {
                             title: {
-                                text: 'ECharts 入门示例'
+                                text: 'Travelers'
                             },
                             tooltip: {},
                             legend: {
                                 data: ['销量']
                             },
                             xAxis: {
-                                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                                text: 'years',
+                                data: response.data.map(function(d) { return d.year })
                             },
-                            yAxis: {},
+                            yAxis: {
+                                text: 'Transported Traveler'
+                            },
                             series: [{
-                                name: '销量',
+                                name: 'Transported Traveler',
                                 type: 'bar',
-                                data: [5, 20, 36, 10, 10, 20]
+                                data: response.data.map(function(d) { return parseInt(d.transportedTraveler) })
                             }]
                         };
                         myChart.setOption(option);
