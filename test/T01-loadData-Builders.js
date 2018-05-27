a444c67c37e440c984c1e1154a4827d206f24c98
sos171810dbd <davidbonilla37@gmail.com>
2018-05-27 09:26:26 +0000

    test

--- test/T01-loadData-Builders.js
+++ test/T01-loadData-Builders.js
@@ -5,7 +5,8 @@
 describe('Data is loaded', function(){
     it('should show some builders', function(){
         browser.
-            get('hhttps://sos1718-10.herokuapp.com/#!/builders')
+            get('https://sos1718-10.herokuapp.com/#!/builders')
+            //get('https://sos171810dbd-sandbox-sos161711dbd.c9users.io/#!/builders')
             .then(function(){
                 element.all(by.repeater('builder in builders'))
                 .then(function (builders) {
