be315823e930493326d1ba367efd5ee7dff96d86
sos171810vcm <viccanmag@alum.us.es>
2018-05-28 10:34:14 +0000

    el toldo la lia

--- test/T01-loadDataBuses.js
+++ test/T01-loadDataBuses.js
@@ -7,14 +7,6 @@ describe('Data is loaded', function() {
     it('should show some buses', function() {
         browser.get(config.getAppUrl()+"/#!/buses").then(function() {
             element.all(by.repeater('bus in buses')).then(function(buses) {
-
-                browser.takeScreenshot().then(function(png) {
-                    var stream = fs.createWriteStream(path.join(process.cwd(), 'test', 'output', 'T01-buses.png'));
-                    stream.write(new Buffer(png, 'base64'));
-                    stream.end();
-
-                });
-
                 expect(buses.length).toBeGreaterThan(0);
 
             });
