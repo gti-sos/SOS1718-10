9aff54325f03c2ee61146d398f48819802c96ab8
sos171810vcm <viccanmag@alum.us.es>
2018-05-28 09:38:36 +0000

    c

--- test/busesConfig.js
+++ test/busesConfig.js
@@ -2,7 +2,7 @@ exports.config={
     
     seleniumAddress:'http://localhost:8910',
     
-    specs:['T01-loadDataBuses.js','T02-addBuses.js'],
+    specs:['T00-apiBuses.js' ,'T01-loadDataBuses.js','T02-addBuses.js'],
     
     capabilities:{
         'browserName':'phantomjs'
