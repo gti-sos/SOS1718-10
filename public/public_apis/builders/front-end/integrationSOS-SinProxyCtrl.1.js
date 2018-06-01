77cc956853b3ee423f528b73b822f7f1d545f90d
sos171810dbd <davidbonilla37@gmail.com>
2018-06-01 16:24:05 +0000

    sin proxy

--- public/public_apis/builders/front-end/integrationSOS-SinProxyCtrl.1.js
+++ public/public_apis/builders/front-end/integrationSOS-SinProxyCtrl.1.js
@@ -43,12 +43,13 @@ angular
                             var nodo = []
                             nodo[0] = buildersCountry[v];
                             responseBuilders.data.forEach((n) => {
+                                if (n.country == buildersCountry[v]) {
+                                    console.log("Country actual " + n.country);
+                                    console.log("Country del array: " + buildersCountry[v]);
+                                    nodo[1] = n.victory;
+                                    console.log("VICTORIAS: " +n.victories)
+                                }
                                 responseUnemployments.data.forEach((p) => {
-                                    if (n.country == buildersCountry[v]) {
-                                        console.log("Country actual " + n.country);
-                                        console.log("Country del array: " + buildersCountry[v]);
-                                        nodo[1] = n.victories;
-                                    }
                                     if (p.country == buildersCountry[v]) {
                                         nodo[2] = p.longterm;
                                     }
@@ -63,16 +64,7 @@ angular
 
                         anychart.onDocumentReady(function() {
                             // create data set on our data
-                            var dataSet = anychart.data.set([
-                                ['GDP', 1, 0.11982978723404256],
-                                ['GDP Real Growth Rate', 0.3666666666666667, 0.5583333333333333],
-                                ['Infant Mortality', 0.06578947368421052, 0.15576923076923077],
-                                ['Life Expectancy', 0.9576093653727663, 0.8268638324091188],
-                                ['Population', 0.22638827767366515, 0.10979008847837246],
-                                ['Area', 0.5390698290165805, 1],
-                                ['Density', 0.02995156531259858, 0.00783120233080335],
-                                ['Population Growth Rate', 0.3087248322147651, -0.12416107382550336]
-                            ]);
+                            var dataSet = anychart.data.set(arrayADevolver);
 
                             // map data for the first series, take x from the zero column and value from the first column of data set
                             var data1 = dataSet.mapAs({
@@ -96,22 +88,12 @@ angular
                             // set chart padding settings
                             chart.padding().bottom(70);
 
-                            // set chart yScale settings
-                            chart.yScale()
-                                .minimum(-.2)
-                                .maximum(1)
-                                .ticks({
-                                    'interval': 0.2
-                                });
-
                             // create first series with mapped data
                             chart.line(data1).name('BUILDERS').markers(true);
                             // create second series with mapped data
                             chart.line(data2).name('UNEMPLYMENTS').markers(true);
-
                             // set tooltip format
                             chart.tooltip().format('Value: {%Value}{decimalsCount: 2}');
-
                             // set container id for the chart
                             chart.container('integrations');
                             // initiate chart drawing
