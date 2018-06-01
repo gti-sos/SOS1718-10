7ec2359537a41faaddf578435448ad2def8c8b13
sos171810vcm <viccanmag@alum.us.es>
2018-06-01 23:49:29 +0000

    xasd

--- public/public_apis/buses/front-end/integracionFinalII.js
+++ public/public_apis/buses/front-end/integracionFinalII.js
@@ -3,7 +3,7 @@
 
 angular
     .module("Principal")
-    .controller("IntegracionFinalI", ["$scope", "$http", function($scope, $http) {
+    .controller("IntegracionFinalII", ["$scope", "$http", function($scope, $http) {
         console.log("integracion controller initialized");
 
         /*eliminar eltos. duplicados*/
