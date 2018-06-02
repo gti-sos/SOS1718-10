9df8ea052d8372d7af71018701244a23dab8f38d
sos171810dbd <davidbonilla37@gmail.com>
2018-06-02 04:16:40 +0000

    as

--- public/public_apis/builders/front-end/apiSOS1.js
+++ public/public_apis/builders/front-end/apiSOS1.js
@@ -6,39 +6,63 @@ controller("ApiSOS1Ctrl", ["$scope", "$http", "$rootScope", function($scope, $ht
     $http.get("/api/v1/builders").then(function(responseBuilders) {
         var builders = responseBuilders.data.map(function(d) { return d.builder })
 
-        $http.get("https://sos1718-03.herokuapp.com/api/v1/pollution-cities").then(function(responsePolutions) {
-                var cars = responsePolutions.data.map(function(d) { return d.car })
-                var array = []
-                for (var i =0; i<builders.length;i++){
-                    var object = {}
-                    object["y"] = cars[i]
-                    object["label"] = builders[i]
-                    array.push(object)
-                } 
+        $http.get("https://sos1718-03.herokuapp.com/api/v1/global-warmings").then(function(responseGlobalWarmings) {
+            var peakPowers = responseGlobalWarmings.data.map(function(d) { return d.peakPower })
+            var array = []
+            for (var i = 0; i < builders.length; i++) {
+                var object = {}
+                object["tittle"] = peakPowers[i]
+                object["value"] = builders[i]
+                array.push(object)
+            }
+
+            var chart = AmCharts.makeChart("apiSOS1", {
+                "type": "funnel",
+                "theme": "light",
+                "dataProvider": array,
+                "balloon": {
+                    "fixedPosition": true
+                },
+                "valueField": "value",
+                "titleField": "title",
+                "marginRight": 240,
+                "marginLeft": 50,
+                "startX": -500,
+                "depth3D": 100,
+                "angle": 40,
+                "outlineAlpha": 1,
+                "outlineColor": "#FFFFFF",
+                "outlineThickness": 2,
+                "labelPosition": "right",
+                "balloonText": "[[title]]: [[value]]n[[description]]",
+                "export": {
+                    "enabled": true
+                }
+            });
 
 
 
 
-                var chart = new CanvasJS.Chart("apiSOS1", {
-                    theme: "light2", // "light1", "light2", "dark1", "dark2"
-                    exportEnabled: true,
-                    animationEnabled: true,
-                    title: {
-                        text: "Desktop Browser Market Share in 2016"
-                    },
-                    data: [{
-                        type: "pie",
-                        startAngle: 25,
-                        toolTipContent: "<b>{label}</b>: {y} cars",
-                        showInLegend: "true",
-                        legendText: "{label}",
-                        indexLabelFontSize: 16,
-                        indexLabel: "{label} - {y}%",
-                        dataPoints: array
-                    }]
-                });
-                chart.render();
+            /*var chart = new CanvasJS.Chart("apiSOS1", {
+                theme: "light2", // "light1", "light2", "dark1", "dark2"
+                exportEnabled: true,
+                animationEnabled: true,
+                title: {
+                    text: "Integrations builders with global-warmings"
+                },
+                data: [{
+                    type: "pie",
+                    startAngle: 25,
+                    toolTipContent: "<b>{label}</b>: {y} peak power",
+                    showInLegend: "true",
+                    legendText: "{label}",
+                    indexLabelFontSize: 16,
+                    indexLabel: "{label} - {y}",
+                    dataPoints: array
+                }]
+            });
+            chart.render();*/
 
-            })
         })
+    })
 }])
