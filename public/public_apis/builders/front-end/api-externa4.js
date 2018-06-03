722289e8fb66cd43892a1b27b169a82952f2df63
sos171810dbd <davidbonilla37@gmail.com>
2018-06-03 15:23:11 +0000

    Ota integracion externa

--- public/public_apis/builders/front-end/api-externa4.js
+++ public/public_apis/builders/front-end/api-externa4.js
@@ -10,11 +10,11 @@ $http.get("/api/v1/builders").then(function(responseBuilders) {
   $http.get("https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=Arsenal").then(function(responseArsenal) {
 
       var idPlayers = []
-      for (var e = 0; e < responseArsenal.data['player'].length; e++) {
-        idPlayers.push(responseArsenal.data['player'].idPlayerManager)
+      for (var e = 0; e < builders.length; e++) {
+        idPlayers.push(parseInt(responseArsenal.data['player'][e].idPlayerManager))
       }
       
-      console.log("idPlayers: " + idPlayers[0])
+      console.log("idPlayers: " + parseInt(responseArsenal.data['player'][0].idPlayerManager))
       
       var datos = []
 
@@ -30,14 +30,15 @@ $http.get("/api/v1/builders").then(function(responseBuilders) {
       google.charts.setOnLoadCallback(drawChart);
       
       var dat = [['Builders', 'idPlayers']]
+      console.log("dat: " + dat)
       dat = dat.concat(datos);
       console.log("dat: " + dat)
 
       function drawChart() {
-        var data = google.visualization.arrayToDataTable([dat]);
+        var data = google.visualization.arrayToDataTable(dat);
 
     var options = {
-      title: 'Lengths of dinosaurs, in meters',
+      title: 'ID players of builders',
       legend: { position: 'none' },
     };
 
--- public/public_apis/builders/front-end/apiSOS3.js
+++ public/public_apis/builders/front-end/apiSOS3.js
@@ -1,18 +1,18 @@
 angular.module("Principal").
 
 controller("ApiSOS3Ctrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope) {
-console.log("Controller initialized (Internal Api 3)");
+  console.log("Controller initialized (Internal Api 3)");
 
-$http.get("/api/v1/builders").then(function(responseBuilders) {
+  $http.get("/api/v1/builders").then(function(responseBuilders) {
 
-  var builders = responseBuilders.data.map(function(d) { return d.builder })
+    var builders = responseBuilders.data.map(function(d) { return d.builder })
 
-  $http.get("https://sos1718-11.herokuapp.com/api/v2/basketball-stats").then(function(responseBasket) {
+    $http.get("https://sos1718-11.herokuapp.com/api/v2/basketball-stats").then(function(responseBasket) {
 
-      var score = responseBasket.data.map(function(d) { return d.first })
-      
+      var score = responseBasket.data.map(function(d) { return parseInt(d.first) })
+
       console.log("idPlayers: " + score)
-      
+
       var datos = []
 
       for (var z = 0; z < builders.length; z++) {
@@ -25,22 +25,24 @@ $http.get("/api/v1/builders").then(function(responseBuilders) {
 
       google.charts.load("current", { packages: ["corechart"] });
       google.charts.setOnLoadCallback(drawChart);
-      
+
       var dat = [['Builders', 'Score']]
+      console.log("dat: " + dat)
       dat = dat.concat(datos);
       console.log("dat: " + dat)
 
       function drawChart() {
-        var data = google.visualization.arrayToDataTable([dat]);
+        var data = google.visualization.arrayToDataTable(dat);
 
-    var options = {
-      title: 'Lengths of dinosaurs, in meters',
-      legend: { position: 'none' },
-    };
+        var options = {
+          title: 'Lengths of dinosaurs, in meters',
+          legend: { position: 'none' },
+        };
 
-    var chart = new google.visualization.Histogram(document.getElementById('apiSOS3')); chart.draw(data, options);
+        var chart = new google.visualization.Histogram(document.getElementById('apiSOS3'));
+        chart.draw(data, options);
 
-  }
-});
-});
+      }
+    });
+  });
 }]);
