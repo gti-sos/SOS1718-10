a0d0f72ab32152463116dd90b8b244be9a6c1682
sos171810fgg <bolodelaus@gmail.com>
2018-06-03 12:52:37 +0000

    ultima hora

--- public/public_apis/motogp/front-end/editCtrl.js
+++ public/public_apis/motogp/front-end/editCtrl.js
@@ -1,26 +1,23 @@
 /* global angular */
 
- angular.module("Principal").controller("EditCtrl1", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
-     console.log("Edit Ctrl initialized!");
-     var pilotURL = "/api/v1/motogp-stats/" + $routeParams.year;
-     
-     $http.get(pilotURL).then(function(response){
-           $scope.updatedPilot = response.data;
-     });
-      $scope.updatePilot = function() {
-      if(Object.values($scope.updatedPilot).includes(null)){
-          $scope.status = "FAIL: It is necesary to fill in all the fields --> status: (400)";
-      }else{
-       $http.put(pilotURL, $scope.updatedPilot).then(function(response) {
-           $scope.status = "UPDATE method Status: Correctly updated (" + response.status + ")";
-           $location.path("/motogp-stats");
-   
-     });
-   
-       
-      }
-  
-      
-     };
-  
- }]);
+angular.module("Principal").controller("EditCtrl1", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
+ console.log("Edit Ctrl initialized!");
+ var pilotURL = "/api/v1/motogp-stats/" + $routeParams.year;
+
+ $http.get(pilotURL).then(function successCallback(response) {
+  $scope.updatedPilot = response.data;
+ });
+ $scope.updatePilot = function errorCallback() {
+  if (Object.values($scope.updatedPilot).includes(null)) {
+   alert("piloto no modificado (rellene todos los campos)");
+  }
+  else {
+   $http.put(pilotURL, $scope.updatedPilot).then(function successCallback(response) {
+    alert("Piloto modificado correctamente");
+    $scope.status = "UPDATE method Status: Correctly updated (" + response.status + ")";
+    $location.path("/motogp-stats");
+
+   });
+  }
+ }
+}]);
